53c538845fb635bc3641309099c5d97673d67a41
Husayin Bahramov <husayinbahramov@gmial.com>
2025-12-15 12:25:38 +0500

    all is true

--- src/components/comps/Navbar.jsx
+++ src/components/comps/Navbar.jsx
@@ -1,9 +1,26 @@
 import React from "react";
 
+// icons
+import { FaShoppingBasket } from "react-icons/fa";
+import { MdMenuOpen } from "react-icons/md";
+
 const Navbar = () => {
   return (
-    <div>
-      <h3>Hello</h3>
+    <div className=" bg-white flex justify-between px-[15px] items-center pb-[5px] border-b-2 rounded-[30px] border-[#9A7447] ">
+      <div>
+        {/* logo */}{" "}
+        <img src="/image512.png" alt="logo" className=" h-[70px]" />{" "}
+      </div>
+      <div className=" flex justify-center items-center gap-[30px]">
+        {/* basket */}
+        <div>
+          <FaShoppingBasket className=" text-[#9A7447] " size={25} />
+        </div>
+        {/* hamburger */}
+        <div>
+          <MdMenuOpen className=" text-[#9A7447] " size={30} />
+        </div>
+      </div>
     </div>
   );
 };
