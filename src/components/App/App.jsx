1a054b7cdd11ddb3ec5c54e36156326f43b239c0
Husayin Bahramov <husayinbahramov@gmial.com>
2025-12-15 12:01:06 +0500

    all is ture

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -27,3 +27,24 @@ const App = () => {
 };
 
 export default App;
+
+// {
+//   "name": "Sanat Hali",
+//   "short_name": "Sanat Hali",
+//   "start_url": "/",
+//   "display": "standalone",
+//   "background_color": "#fff7ed",
+//   "theme_color": "#fff7ed",
+//   "icons": [
+//     {
+//       "src": "/image192.jpg",
+//       "sizes": "192x192",
+//       "type": "image/jpg"
+//     },
+//     {
+//       "src": "/image512.jpg",
+//       "sizes": "512x512",
+//       "type": "image/jpg"
+//     }
+//   ]
+// }
